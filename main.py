0bc24bcede50ec106819ba001ffecb88922e03ed
Jay Sim <yourthought2020@gmail.com>
2023-08-28 15:25:58 -0700

    Add pdf.line

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100,100,100)
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="M", ln=1)
+    pdf.line(10, 21, 200, 21)
+
 
 pdf.output("output.pdf")
