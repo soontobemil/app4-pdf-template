25462b6bc9ec0df15f7ac8e3840ff28700532b3c
Jay Sim <yourthought2020@gmail.com>
2023-08-28 15:13:54 -0700

    Import pandas module

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+df = pd.read_csv("topics.csv")
 pdf.add_page()
 
 pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello there!", align="L", ln=1, border=1)
-pdf.cell(w=0, h=12, txt="Hi there!", align="L", ln=1, border=1)
+pdf.cell(w=0, h=12, txt="Hello there!", align="M", ln=1)
+pdf.set_font(family="Times", size=10)
+pdf.cell(w=0, h=12, txt="Hi there!", align="L", ln=1)
 
 pdf.output("output.pdf")
