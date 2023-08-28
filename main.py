3d8379e255d07d5b86ddb7f50fd478f3f394802b
Jay Sim <yourthought2020@gmail.com>
2023-08-28 15:20:56 -0700

    Use interrows to loop over the topics in topics.csv file

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
-pdf.add_page()
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello there!", align="M", ln=1)
-pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hi there!", align="L", ln=1)
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="M", ln=1)
 
 pdf.output("output.pdf")
